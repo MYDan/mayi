69c59156c440bd5eaf042c0507cc76a086e69c04
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-06-08 23:28:01 +0800

    update

--- lib/MYDan/API.pm
+++ lib/MYDan/API.pm
@@ -7,10 +7,6 @@ use JSON;
 use LWP::UserAgent;
 use MYDan::Util::OptConf;
 use URI::Escape;
-use Sys::Hostname;
-use HTTP::Cookies;
-use MYDan::Node;
-use Net::Ping;
 use YAML::XS;
 
 my $o; BEGIN{ $o = MYDan::Util::OptConf->load()->dump( 'api' ); }; 
@@ -19,12 +15,10 @@ sub new
 {
     my ( $class, %self ) = splice @_;
 
-    $self{addr} ||=  $o->{addr};
-    confess "api addr undef" unless $self{addr};
+    confess "api addr undef" unless $self{addr} ||= $o->{addr};
 
     $self{ua} = my $ua = LWP::UserAgent->new();
     $ua->agent('Mozilla/9 [en] (Centos; Linux)');
-
     $ua->timeout( 10 );
 
     bless \%self, ref $class || $class;
@@ -34,41 +28,39 @@ sub _get
 {
     my ( $self, $uri ) = @_;
 
-#    $uri = URI::Escape::uri_escape( $uri );
-    $uri =~ s/ /%20/g;
+    $uri = URI::Escape::uri_escape( $uri );
 
     my $url = "$self->{addr}$uri";
     return +{ stat => JSON::true, data => $url } if $self->{urlonly};
 
     my $res = $self->{ua}->get( $url );
-    system "touch '$self->{mark}'" if $res->code() == 500 && $self->{mark} && ! $ENV{MYDan_OpenAPI_Retry};
-    print "get $url\n" if $ENV{MYDan_DEBUG};
     my $cont = $res->content;
     return +{ stat => JSON::false, info => $res->content } unless $res->is_success;
 
-    my $data = eval{JSON::from_json $cont};
+    my $data = eval{ JSON::from_json $cont };
     return $@ ? +{ stat => JSON::false, info => $@ } : $data;
 }
 
 sub get
 {
-    my $self = shift;
+    my ( $self, $uri ) = @_;
 
-    my ( $uri ) = @_;
+    my $error = "openapi fail: $uri\n";
+
     if( $ENV{MYDan_OpenAPI_Retry} )
     {
         while( 1 )
         {
-            my $res = $self->_get( @_ );
+            my $res = $self->_get( $uri );
             return $res->{data} if $res->{stat};
-            warn "[WARN] openapi fail: $uri\n";
+            warn "[WARN] $error";
             sleep 3;
         }
     }
     else
     {
         my $res = $self->_get( @_ );
-        $res->{stat} ? $res->{data} : die "[ERROR] openapi fail: $uri\n";
+        $res->{stat} ? $res->{data} : die "[ERROR] $error";
     }
 }
 
@@ -76,8 +68,7 @@ sub _post
 {
     my ( $self, $uri, %form ) = @_;
 
-#    $uri = URI::Escape::uri_escape( $uri );
-    $uri =~ s/ /%20/g;
+    $uri = URI::Escape::uri_escape( $uri );
 
     my $url = "$self->{addr}$uri";
     return +{ stat => JSON::true, data => $url } if $self->{urlonly};
@@ -112,6 +103,52 @@ sub post
     else
     {
         my $res = $self->_post( @_ );
+        $res->{stat} ? $res->{data} : die "[ERROR] openapi fail: $uri\n";
+    }
+
+}
+
+sub _stream
+{
+    my ( $self, $uri, $stream ) = @_;
+
+#    $uri = URI::Escape::uri_escape( $uri );
+    $uri =~ s/ /%20/g;
+
+    my $url = "$self->{addr}$uri";
+    return +{ stat => JSON::true, data => $url } if $self->{urlonly};
+
+    print "post $url\n" if $ENV{MYDan_DEBUG};
+    my $res = $self->{ua}->post( "$url", 
+          Content => $stream, 'Content-Type' => 'application/octet-stream' );
+
+    system "touch '$self->{mark}'" if $res->code() == 500 && $self->{mark} && ! $ENV{MYDan_OpenAPI_Retry};
+    my $cont = $res->content;
+    return +{ stat => JSON::false, info => $res->content } unless $res->is_success;
+
+    my $data = eval{JSON::from_json $cont};
+    return $@ ? +{ stat => JSON::false, info => $@ } : $data;
+
+}
+
+sub stream
+{
+    my $self = shift;
+
+    my ( $uri ) = @_;
+    if( $ENV{MYDan_OpenAPI_Retry} )
+    {
+        while( 1 )
+        {
+            my $res = $self->_stream( @_ );
+            return $res->{data} if $res->{stat};
+            warn "[WARN] openapi fail: $uri\n";
+            sleep 3;
+        }
+    }
+    else
+    {
+        my $res = $self->_stream( @_ );
         $res->{stat} ? $res->{data} : die "[ERROR] openapi fail: $uri\n";
     }
 
