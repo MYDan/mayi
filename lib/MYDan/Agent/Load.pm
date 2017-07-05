4d7c6603967c4ad304f538c15d2c3c093750dcb9
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-07-05 23:51:21 +0800

    update

---
+++ lib/MYDan/Agent/Load.pm
@@ -0,0 +1,134 @@
+package MYDan::Agent::Load;
+
+=head1 NAME
+
+MYDan::Agent::Load
+
+   load data by agent
+
+=head1 SYNOPSIS
+
+ use MYDan::Agent::Load; my $load = MYDan::Agent::Load->new( 
+   node => 'host1', sp=> 'srcpath', dp => 'dstpath'
+ );
+
+ my %result = $client->load( 
+   timeout => 300,
+   user => '',
+   sudo => '',
+   verbose => 1, 
+   port => '',
+   continue => 0,
+ ); 
+
+=cut
+use strict;
+use warnings;
+
+use Carp;
+use File::Spec;
+use MYDan::Util::OptConf;
+use MYDan::Agent::Client;
+use Time::HiRes qw(time);
+use AnyEvent;
+use AnyEvent::Impl::Perl;
+use AnyEvent::Socket;
+use AnyEvent::Handle;
+use Digest::MD5;
+use MYDan::Agent::Query;
+use Fcntl qw(:flock SEEK_END);
+
+sub new
+{
+    my ( $class, %self ) = @_;
+    map{ confess "$_ undef" unless $self{$_} }qw( node sp );
+
+    $self{dp} ||= $self{sp};
+
+    bless \%self, ref $class || $class;
+}
+
+
+sub run
+{
+    my ( $this, %run ) = @_;
+
+    my ( $node, $sp, $dp ) = @$thisqw{qw( node sp dp )};
+
+    my $temp = sprintf "$dp.%stmp", $run{continue} ? '' : time.'.'.$$.'.';
+
+    my $position = -f $temp : ( stat $temp )[7] : 0;
+
+    open my $TEMP, '+>', $temp or die "Can't open '$temp': $!";
+
+    my $query = MYDan::Agent::Query->dump(
+        +{ code => 'load', user => $run{user}, sudo=> $run{sudo} argv => [ $sp, $position ] }
+    );
+
+    my ( $cv, $len, %keepalive )
+        = ( AE::cv, 0,  cont => '', skip => 0, first => 1 );
+
+    tcp_connect $node, $run{port}, sub {
+        my ( $fh ) = @_  or die "tcp_connect: $!";
+        my $hdl; $hdl = new AnyEvent::Handle(
+           fh => $fh,
+           on_read => sub {
+               my $self = shift;
+               printf "load byte: %s\n", length $self->{rbuf} if $run{verbose};
+               $self->unshift_read (
+                   chunk => length $self->{rbuf},
+                   sub {
+		       ( $keepalive{first}, $keepalive{skip} ) = ( 0, 1 )
+                           if $keepalive{first} && $_[1] !~ /^\*/;
+
+                       if( $keepalive{skip} )
+                       {
+                           $len += length $_[1];
+                           print $TMP $_[1];
+                       }
+                       else
+                       {
+                           $keepalive{cont} .= $_[1];
+                           $keepalive{cont} =~ s/^\*+//g;
+                           if( $keepalive{cont} =~ s/^#\*keepalive\*#// )
+                           {
+                               $keepalive{skip} = 1;
+                               $len += length $keepalive{cont};
+                               print $TMP $keepalive{cont};
+                           }
+                       }
+                   }
+               );
+            },
+            on_eof => sub{
+                undef $hdl;
+                 $cv->send;
+             }
+        );
+        $hdl->push_write($query);
+        $hdl->push_shutdown;
+    };
+
+    $cv->recv;
+    
+    seek( $TEMP, -38, SEEK_END );
+    my $end = ''; while( <$TEMP> ) { $end .= $_; }
+    my @end = $end =~ /^([0-9a-z]{32})--- (\d+)\n/;
+    unless( defined $end[1] && $end[1] == 0 )
+    {
+        unlink  $temp;
+        die "call agent fail: end=$end\n";
+    }
+    truncate $TEMP, $len - 38;
+    seek $TEMP, 0, 0;
+
+    my $md5 = Digest::MD5->new()->addfile( $TEMP )->hexdigest();
+    unless( defined $end[0] && $end[0] eq $md5 )
+    {
+        unlink  $temp;
+        die "md5 nomatch\n";
+    }
+    rename $temp, $dp;
+}
+
+1;
