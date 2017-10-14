ebe514daf168caff404a50af957518d829884047
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-10-14 23:23:05 +0800

    bin/mydan 添加dan和box之间的切换开关

--- lib/MYDan/Util/Command.pm
+++ lib/MYDan/Util/Command.pm
@@ -16,13 +16,28 @@ sub do
 
     $ENV{MYDan_DEBUG} = 1 && $cmd = lc( $cmd ) if $cmd && $cmd =~ /^[A-Z][a-z]*$/;
 
+    if( $cmd && $cmd eq '--box' )
+    {
+        system "touch '$MYDan::PATH/dan/.ignore'" if -d "$MYDan::PATH/dan";
+        print "Switch to `box` first.\n";
+	return;
+    }
+    elsif( $cmd && $cmd eq '--dan' )
+    {
+        unlink "$MYDan::PATH/dan/.ignore" if -f "$MYDan::PATH/dan/.ignore";
+        print "Switch to `dan` first.\n";
+	return;
+    }
+
     $cmd = $this->{alias}{$cmd} if $cmd && $this->{alias}{$cmd};
 
     my ( $c ) = grep{ $cmd && $_->[0] eq $cmd }@{$this->{cmd}};
 
     $this->help() and return unless $c;
 
-    map{ exec join( ' ', "$MYDan::PATH/$_", map{"'$_'"}@argv ) if -e "$MYDan::PATH/$_" }splice @$c, 2;
+    my @x = splice @$_, 2;
+    @x = reverse @x if -f "$MYDan::PATH/dan/.ignore";
+    map{ exec join( ' ', "$MYDan::PATH/$_", map{"'$_'"}@argv ) if -e "$MYDan::PATH/$_" }@x;
     print "$cmd is not installed\n";
 }
 
@@ -32,10 +47,15 @@ sub help
 
     my ( $name, $cmd ) = @$this{qw( name cmd )};
 
-    print "Usage: $name COMMAND [arg...]\n\nCommands:\n";
+    print "Usage: $name COMMAND [arg...]\n";
+    print "Options:\n\t--dan\tSwitch to `dan` first.\n\t--box\tSwitch to `box` first.\n\n";
+    print "\tHelp\tshow detail\n";
+    print "Commands:\n";
 
     map{
-        my $x = grep{ -e "$MYDan::PATH/$_" }splice @$_, 2;
+        my @x = splice @$_, 2;
+	@x = reverse @x if -f "$MYDan::PATH/dan/.ignore";
+        my ( $x ) = grep{ -e "$MYDan::PATH/$_" }@x;
 	if( $ENV{MYDan_DEBUG} )
 	{
             printf "\t%s $_->[0]\t$_->[1]\n", $x ? $x =~ /^dan/ ? 'dan' : 'box' : 'nil';
