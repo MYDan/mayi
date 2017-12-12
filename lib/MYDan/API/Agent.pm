6c2733e821eca586ab730aeec4fb0fa1569e028d
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-12 20:46:34 +0800

    添加mydan.encryption判断,如果安装了mydan.encryption则从mydan.encryption获取权限

--- lib/MYDan/API/Agent.pm
+++ lib/MYDan/API/Agent.pm
@@ -3,16 +3,30 @@ use strict;
 use warnings;
 use Carp;
 
-
 use  Compress::Zlib;
 
 use base qw( MYDan::API );
 
 our $URI = "/api/v1/agent";
+use MYDan;
+our $code;
 
 sub encryption
 {
     my ( $self, $data, $temp ) = splice @_, 0, 2;
+
+    unless( defined $code )
+    {
+        my $e = "/etc/mydan.encryption/main";
+        if( -e $e )
+        {
+            my $c = do $e;
+            die "load $e error" unless $c && ref $c eq 'CODE';
+            $code = $c;
+        }
+        else { $code = 0; }
+    }
+
 
     if ( $data =~ s/^data:(\d+):// )
     {
@@ -20,7 +34,7 @@ sub encryption
         substr( $data, 0, $1 ) = '';
     }
 
-    my $raw = $self->_stream( "$URI/encryption", $data );
+    my $raw = $code ? &$code( $data ) : $self->_stream( "$URI/encryption", $data );
     my $d = Compress::Zlib::uncompress( $raw );
     die "$URI/encryption fail:$raw" unless $d; 
     return ( $temp || '' ) . $raw;
