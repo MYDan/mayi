47a3820ca5eb59ab48167d1fcd6bf6952f7c7c98
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-06-13 23:32:00 +0800

    antdencli只允许操作自己的资源

--- lib/MYDan/API/AntDen.pm
+++ lib/MYDan/API/AntDen.pm
@@ -3,41 +3,42 @@ use strict;
 use warnings;
 
 use base qw( MYDan::API );
-our $URI = "";
+our $URI = "/api/antdencli";
 
 sub submitjob
 {
     my ( $self, %data ) = @_;
-    return $self->post( "$URI/scheduler/submitJob", %data );
+    return $self->post( "$URI/submitJob", %data );
 }
 
 sub listjob
 {
     my ( $self, $owner ) = @_;
-    return $self->get( "$URI/scheduler/listJob?owner=$owner" );
+    return $self->get( "$URI/listJob?owner=$owner" );
 }
 
 sub info
 {
-    my ( $self, $jobid ) = @_;
-    return $self->get( "$URI/scheduler/jobinfo/$jobid" );
+    my ( $self, $jobid, $owner ) = @_;
+    return $self->get( "$URI/jobinfo/$jobid?owner=$owner" );
 }
 
 sub stop
 {
-    my ( $self, $jobid ) = @_;
-    return $self->get( "$URI/scheduler/jobstop/$jobid" );
+    my ( $self, $jobid, $owner ) = @_;
+    return $self->get( "$URI/jobstop/$jobid?owner=$owner" );
 }
 
 sub taskinfo
 {
     my ( $self, $taskid ) = @_;
-    return $self->get( "$URI/scheduler/taskinfo/$taskid" );
+    return $self->get( "$URI/taskinfo/$taskid" );
 }
 
 sub resources
 {
-    return shift->get( "$URI/scheduler/resources" );
+    my ( $self, $owner ) = @_;
+    return shift->get( "$URI/resources?owner=$owner" );
 }
 
 1;
